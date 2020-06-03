32f7eefc1a3e9ce55bd02bfa892b76de7ba4057e
DimaDennys <dumitrudenisiuc@gmail.com>
2020-06-03 12:20:13 +0100

    add module2

---
+++ module2.js
@@ -0,0 +1,5 @@
+var a = "Hello from module2!";
+
+module.exports.a = a;
+module.exports.b = 2;
+exports.c = "shorthand";
--- modules.js
+++ modules.js
@@ -2,4 +2,8 @@
 
 console.log("hi");
 var a = 1;
-console.log(a);
+console.log(a); 
+
+var m2 = require('./module2');
+console.log(m2);
+console.log(m2.b);
